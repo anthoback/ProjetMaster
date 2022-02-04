dfd69f2d9b2f5c3325627659049e90b3a9eaad48
anthoback <44365380+anthoback@users.noreply.github.com>
2022-02-04 12:06:19 +0100

    Update AnalyseMSSuspect.py

--- Script MS/AnalyseMSSuspect.py
+++ Script MS/AnalyseMSSuspect.py
@@ -1,15 +1,18 @@
 import sys,os,string
 import time
 
+#path of this file
+chemin = os.path.dirname(__file__)
+
 def intiFileCode() :
-		#create the file or empty it
-	with open("FichierTXT/CodeSuspect.txt", 'w+') as f:
+	#create the file or empty it
+	with open(chemin+"/FichierTXT/CodeSuspect.txt", 'w+') as f:
 	    f.truncate(0)
 	f.close
 
 def afficheFile() :
 	#open the file and write the lines that interest us
-	with open("FichierTXT/CodeSuspect.txt") as content:
+	with open(chemin+"/FichierTXT/CodeSuspect.txt") as content:
 		YouCanRead = 0
 		code = content.readlines()
 		for l in code:
@@ -17,7 +20,7 @@ def afficheFile() :
 
 def main() :
 	FileMS = 0
-	with open("FichierTXT/FileSuspect.txt") as text:
+	with open(chemin+"/FichierTXT/FileSuspect.txt") as text:
 		datafile = text.readlines()
 		#read all lines assumed to be paths
 		for line in datafile:
@@ -30,27 +33,30 @@ def main() :
 			 #use the tool and save the results in CodeSuspect
 			for m in range (1,len(l)) :
 				print("-------------------------------------------------------------------------------------------\n"+ 'Fichier concerné :' + f + '\nFlux concerné :' + l[m] + "\n\n")
-				os.system('python3 oledump.py -s '+l[m] +' -v ' + f + ' > FichierTXT/CodeSuspect.txt' )
+				os.system('python3 "'+chemin+'/oledump.py" -s '+l[m] +' -v ' + f + ' > "'+chemin+'/FichierTXT/CodeSuspect.txt"' )
 				afficheFile()	
 
 	return FileMS
 
 
 if __name__ == '__main__':
-
    
-    print("\nSTART RESEARCH\n")
-    #start the timer
-    start = time.time()
-    
     intiFileCode()
-    MS = main()
     
-    #end the timer
-    end = time.time()
-    print("\nFINISH\n")
+    if os.path.getsize(chemin+"/FichierTXT/FileSuspect.txt") != 0 :
+        print("\nSTART RESEARCH\n")
+    	#start the timer
+        start = time.time()
+
+        MS = main()
+
+    	#end the timer
+        end = time.time()
+        print("\nFINISH\n")
+	    
+		#print some performance information
+        print("Temps :" + str(int(end-start)) + " secondes")
+        print("Nombre de fichier MS: " + str(MS))
     
-    #print some performance information
-    print("Temps :" + str(int(end-start)) + " secondes")
-    print("Nombre de fichier MS: " + str(MS))
-   
+    else : 
+        print("\nFileSuspect Vide")
