c70a5a76c02c29d8a1abeb81331fbf181d9e7fb3
anthoback <44365380+anthoback@users.noreply.github.com>
2022-02-12 11:22:15 +0100

    Update OutilAnalyseFichier.py

--- OutilAnalyseFichier.py
+++ OutilAnalyseFichier.py
@@ -1,14 +1,14 @@
 import sys,os,string
 
 if __name__ == '__main__':
-	print("Cette outil a pour but de parcourir les différents fichier pour but de parcourir les différents fichier d'un dossier pour pouvoir découvrir des fichier malveillant PDF ou OLE")
+	print("Cette outil a pour but de parcourir les différents fichiers d'un dossier pour pouvoir découvrir des fichiers malveillants PDF ou OLE")
 	
 	
 	exit = 1
 	while exit :
 		print("\n 1 : Chercher des fichiers malicieux PDF \n 2 : Chercher des fichiers malicieux OLE  \n 3 : exit")
 
-		choix = int(input("\nQuel est ton choix ? "))
+		choix = int(input("\nQuel est le choix ? "))
 		if choix == 1 :
 			os.system('python "Script PDF/SearchFileMaliciousPDF.py" ')
 			
